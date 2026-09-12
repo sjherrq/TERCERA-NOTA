857923084d1bdf7de0c75aa3054fb155ae499c68
sjherrq <sjherreraq@ufpso.edu.co>
2026-09-12 07:39:24 -0500

    comenzar ejercicio combinado

--- ejercicio combinado/ejercicio_combinado/src/combinados.java
+++ ejercicio combinado/ejercicio_combinado/src/combinados.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class combinados {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //Instrucción: Crea un programa que solicite al usuario su nombre,
+        //edad, y altura, y luego muestre un mensaje personalizado que incluya todos esos datos.
+
+        Scanner leer = new Scanner (System.in);
+        System.out.println("ingrese su nombre");
+        String nombre = leer.nextLine();
+
+
+
+
+
+
+
+
     }
 }
