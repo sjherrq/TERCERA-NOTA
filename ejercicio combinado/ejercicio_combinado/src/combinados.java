95d16c0f6259a5b1ce9fc7bfb07203efa4946c07
sjherrq <sjherreraq@ufpso.edu.co>
2026-09-15 06:11:34 -0500

    terminar ejercicio combinado

--- ejercicio combinado/ejercicio_combinado/src/combinados.java
+++ ejercicio combinado/ejercicio_combinado/src/combinados.java
@@ -8,14 +8,16 @@ public class combinados {
         //edad, y altura, y luego muestre un mensaje personalizado que incluya todos esos datos.
 
         Scanner leer = new Scanner (System.in);
-        System.out.println("ingrese su nombre");
+        System.out.println("Ingrese su nombre");
         String nombre = leer.nextLine();
+        System.out.println("Ingrese su edad");
+        byte edad = leer.nextByte();
+        System.out.println("Ingrese su tamaño en cm");
+        short tamaño = leer.nextShort();
 
+        System.out.println("Bienvenido "+nombre+" tienes "+edad+" años y mides "+tamaño+"cm");
 
-
-
-
-
+        leer.close();
 
 
     }
