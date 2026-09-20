cfaf34da1c4cd70ae57873dc105c31a19d9e8452
sjherrq <sjherreraq@ufpso.edu.co>
2026-09-20 14:18:39 -0500

    terminar factorial

---
+++ Factorial/factorial/src/Factorial.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) throws Exception {
+       
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Porfavor ingresa un numero para calcular su factorial.");
+        int entrada = teclado.nextInt();
+
+        int factorial = 1;
+        int i = 1;
+
+
+
+        while (i <= entrada) {
+            factorial *= i;
+            i++;
+
+
+             
+
+        }
+
+           System.out.println(factorial);
+
+
+
+
+
+
+
+
+        teclado.close();
+
+    }
+}
