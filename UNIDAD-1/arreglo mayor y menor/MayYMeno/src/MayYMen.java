c4ca939c52e449c8b7a33e4e6da6588c4af1b3fe
sjherrq <sjherreraq@ufpso.edu.co>
2026-09-21 22:21:53 -0500

    hallar mayor y menor matriz

---
+++ UNIDAD-1/arreglo mayor y menor/MayYMeno/src/MayYMen.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class MayYMen {
+    public static void main(String[] args) throws Exception {
+    Scanner teclado= new Scanner(System.in);
+    int[][] matriz = new int[4][4];
+    int CANTFILAS=4;
+    int CANTCOLUM=4;   
+    int mayor=0;
+    int menor=0;
+    int[] PosMay=new int[2];
+    int[] PosMeno=new int[2];
+    for (int filas = 0; filas < CANTFILAS; filas++)  {
+      
+        for (int columnas = 0; columnas < CANTCOLUM; columnas++)  {
+            
+            System.out.println("Ingrsa los valores de la fila "+ (filas+1)+ " y columna "+(columnas+1));
+            matriz[filas][columnas] = teclado.nextInt(); 
+            
+            
+            if (mayor<matriz[filas][columnas]) {
+                mayor=matriz[filas][columnas];
+                PosMay[0]=(filas+1);
+                PosMay[1]=(columnas+1);
+            }
+            if (menor ==0){
+                menor=mayor;
+            }
+             if (matriz[filas][columnas]<menor) {
+                menor=matriz[filas][columnas];
+                PosMeno[0]=(filas+1);
+                PosMeno[1]=(columnas+1);
+            } 
+
+
+
+
+        }
+    }
+
+
+
+
+
+for (int filas = 0; filas < CANTFILAS; filas++) {
+    for (int columnas = 0; columnas < CANTCOLUM;    columnas++) {
+        System.out.print(matriz[filas][columnas]+" ");
+    }
+    System.out.println();
+}
+
+    System.out.println("el mayor es "+ mayor+ " en la fila "+PosMay[0]+" y columna "+PosMay[1]);
+    System.out.println("el menor es "+ menor+ " en la fila "+PosMeno[0]+" y columna "+PosMeno[1] );
+
+
+
+    teclado.close();
+
+    }
+}
